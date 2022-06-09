89d5c469654e4089ebb12ee757cc00fe1e9b83f2
ivancalarde1301 <ivan.calarde1301@gmail.com>
2022-06-10 00:17:37 +0800

    UI Configuration

---
+++ Contact Tracing App/Contact Tracing App/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Tracing_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\ivanc\Downloads\test.txt", true);
+            file.WriteLine("Welcome to my Contact Tracing App");
+            file.Close();
+        }
+    }
+}
